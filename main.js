6f6c04473eb571933d4f30f0b19715b23a537431
ishant96 <85052977+ishant96@users.noreply.github.com>
2021-12-21 18:35:56 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,247 @@
+
+/*created by prashant shukla */
+img = "";
+noseX = 0;
+noseY = 0;
+marioX = 325;
+marioY = 325;
+
+var paddle2 =10,paddle1=10;
+
+var paddle1X = 10,paddle1Height = 110;
+var paddle2Y = 685,paddle2Height = 70;
+
+var score1 = 0, score2 =0;
+var paddle1Y;
+
+var  playerscore =0;
+var audio1;
+var pcscore =0;
+//ball x and y and speedx speed y and radius
+var ball = {
+    x:350/2,
+    y:480/2,
+    r:20,
+    dx:3,
+    dy:3
+}
+
+function setup(){
+  var canvas =  createCanvas(700,600);
+}
+
+
+function draw(){
+
+ background(0); 
+
+ fill("black");
+ stroke("black");
+ rect(680,0,20,700);
+
+ fill("black");
+ stroke("black");
+ rect(0,0,20,700);
+ 
+   //funtion paddleInCanvas call 
+   paddleInCanvas();
+ 
+   //left paddle
+   fill(250,0,0);
+    stroke(0,0,250);
+    strokeWeight(0.5);
+   paddle1Y = mouseY; 
+   rect(paddle1X,paddle1Y,paddle1,paddle1Height,100);
+   
+   
+    //pc computer paddle
+    fill("#FFA500");
+    stroke("#FFA500");
+   var paddle2y =ball.y-paddle2Height/2;  rect(paddle2Y,paddle2y,paddle2,paddle2Height,100);
+    
+    //function midline call
+    midline();
+    
+    //funtion drawScore call 
+   drawScore();
+   
+   //function models call  
+   models();
+   
+   //function move call which in very important
+    move();
+}
+
+
+
+//function reset when ball does notcame in the contact of padde
+function reset(){
+   ball.x = width/2+100,
+   ball.y = height/2+100;
+   ball.dx=3;
+   ball.dy =3;
+   
+}
+
+
+//function midline draw a line in center
+function midline(){
+    for(i=0;i<480;i+=10) {
+    var y = 0;
+    fill("white");
+    stroke(0);
+    rect(width/2,y+i,10,480);
+    }
+}
+
+
+//function drawScore show scores
+function drawScore(){
+    textAlign(CENTER);
+    textSize(20);
+    fill("white");
+    stroke(250,0,0)
+    text("Player:",100,50)
+    text(playerscore,140,50);
+    text("Computer:",500,50)
+    text(pcscore,555,50)
+}
+
+
+//very important function of this game
+function move(){
+   fill(50,350,0);
+   stroke(255,0,0);
+   strokeWeight(0.5);
+   ellipse(ball.x,ball.y,ball.r,20)
+   ball.x = ball.x + ball.dx;
+   ball.y = ball.y + ball.dy;
+   if(ball.x+ball.r>width-ball.r/2){
+       ball.dx=-ball.dx-0.5;       
+   }
+  if (ball.x-2.5*ball.r/2< 0){
+  if (ball.y >= paddle1Y&& ball.y <= paddle1Y + paddle1Height) {
+    ball.dx = -ball.dx+0.5; 
+  }
+  else{
+    pcscore++;
+    reset();
+    navigator.vibrate(100);
+  }
+}
+if(pcscore ==4){
+    fill("#FFA500");
+    stroke(0)
+    rect(0,0,width,height-1);
+    fill("white");
+    stroke("white");
+    textSize(25)
+    text("Game Over!☹☹",width/2,height/2);
+    text("Reload The Page!",width/2,height/2+30)
+    noLoop();
+    pcscore = 0;
+}
+   if(ball.y+ball.r > height || ball.y-ball.r <0){
+       ball.dy =- ball.dy;
+   }   
+}
+
+
+//width height of canvas speed of ball 
+function models(){
+    textSize(18);
+    fill(255);
+    noStroke();
+    text("Width:"+width,135,15);
+    text("Speed:"+abs(ball.dx),50,15);
+    text("Height:"+height,235,15)
+}
+
+
+//this function help to not go te paddle out of canvas
+function paddleInCanvas(){
+  if(mouseY+paddle1Height > height){
+    mouseY=height-paddle1Height;
+  }
+  if(mouseY < 0){
+    mouseY =0;
+  }  
+}
+
+
+function startGame()
+{
+  GameStatus = "start";
+  document.getElementById("status").innerHTML = "Game Is Loading";
+}
+function game(){
+console.log("noseX = " + noseX +" ,noseY = "+noseY);
+  instializeInDraw();
+  moveEnvironment(mario);
+  drawSprites();
+  
+  if(gameConfig.status==='start'){
+
+    fill(0,0,0,150);
+    rect(0,0,gameConfig.screenX,gameConfig.screenY);
+
+    fill(255, 255, 255);
+    textSize(40);
+    textAlign(CENTER);
+    text("Press Play Button To Start The Game", gameConfig.screenX/2, gameConfig.screenY/2);
+    textSize(40);
+
+    stroke(255);
+    strokeWeight(7);
+    noFill();
+
+    changeGameStatud();
+  }
+  
+  if(gameConfig.status==='play'){
+    positionOfCharacter(mario);
+    enemys(enemyMushrooms);
+    checkStatus(mario);
+    scores(mario);
+    manualControl(mario);
+  
+    // optional control version of game
+    // autoControl(mario);
+  
+  }
+
+    // if game is over 
+  if(gameConfig.status==='gameover'){
+    fill(0,0,0,150);
+    rect(0,0,gameConfig.screenX,gameConfig.screenY);
+
+    fill(255, 255, 255);
+    textSize(40);
+    textAlign(CENTER);
+    text("GAME OVER", gameConfig.screenX/2, gameConfig.screenY/2+105);
+    textSize(15);
+    text("Press SPACE to Restart", gameConfig.screenX/2, gameConfig.screenY/2+135);
+    textSize(40);
+    text(round(gameConfig.scores),gameConfig.screenX/2,gameConfig.screenY/2-35);
+    text("points",gameConfig.screenX/2,gameConfig.screenY/2);
+
+    stroke(255);
+    strokeWeight(7);
+    noFill();
+    ellipse(gameConfig.screenX/2,gameConfig.screenY/2-30,160,160)
+    changeGameStatud(mario)
+  }
+}  
+
+
+// change game status if any key is pressed
+function changeGameStatud(character){
+  if(GameStatus=="start"&& noseX !="" && gameConfig.status==="start") {
+    world_start.play();
+    initializeCharacterStatus(mario);
+    gameConfig.status= "play";
+  }
+  if(gameConfig.status==="gameover" && keyDown(control.revive)) {
+    gameConfig.status= "start";        
+  }
+}
